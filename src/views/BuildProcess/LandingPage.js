38d17e72e03229869c3fdbf607c99e9c83bb9402
bqardi <51706945+bqardi@users.noreply.github.com>
2021-08-05 18:26:00 +0200

    Landing page process

---
+++ src/views/BuildProcess/LandingPage.js
@@ -0,0 +1,19 @@
+import { useStoreContext } from "../../components/Store";
+import Layout from "../../layout/Layout";
+
+function LandingPage(){
+	var {Translate} = useStoreContext();
+	
+	return (
+		<Layout.Cell modifiers={["padding", "child-margin"]}>
+			<h3 className="BuildProcess__titleAccordion">
+				<Translate id="title-01" extended="build-process-landing-page" />
+			</h3>
+			<p className="BuildProcess__text">
+				<Translate id="title-01-p-01" extended="build-process-landing-page" />
+			</p>
+		</Layout.Cell>
+	);
+}
+
+export default LandingPage;
