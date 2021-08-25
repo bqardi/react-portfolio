2ba13acbcf1312eff385824e9a9b0b53af95e301
bqardi <51706945+bqardi@users.noreply.github.com>
2021-08-25 09:43:27 +0200

    Bloom added to online profiles.

--- src/components/OnlineProfiles.js
+++ src/components/OnlineProfiles.js
@@ -6,15 +6,21 @@ function OnlineProfiles() {
 		<article className="OnlineProfiles">
 			<ul>
 				<li>
-					Smartr:{" "}
+					Smartr:&nbsp;
 					<Button href="https://www.smartr.me/public/profiles/sune.seifert">
 						https://www.smartr.me/public/profiles/sune.seifert
 					</Button>
 				</li>
 				<li>
-					iHeadHunt:{" "}
+					iHeadHunt:&nbsp;
 					<Button href="https://jobs.iheadhunt.dk/">
 						https://jobs.iheadhunt.dk/
+					</Button>
+				</li>
+				<li>
+					Bloom:&nbsp;
+					<Button href="https://bloom.dk/">
+						https://bloom.dk/
 					</Button>
 				</li>
 			</ul>
