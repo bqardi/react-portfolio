5921ba31a58b1f761ee4edecfcefff211254765f
bqardi <51706945+bqardi@users.noreply.github.com>
2021-08-25 09:52:23 +0200

    Form validation, error position

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,40 +32,44 @@ function Form() {
 	return (
 		<form action="" className="Form" onSubmit={submitHandler} noValidate>
 			<Layout.Flex column gap="8px">
-				<label htmlFor="name">
-					<Translate id="contact-name" />
-				</label>
-				<input
-					id="name"
-					name="name"
-					type="text"
-					value={values.name}
-					onChange={changeHandler}
-					className={
-						errors.name ? "Form__input Form__input--error" : "Form__input"
-					}
-					required
-				/>
-				{errors.name && (
-					<p className="Form__error">{<Translate id={errors.name} />}</p>
-				)}
-				<label htmlFor="email">
-					<Translate id="contact-email" />
-				</label>
-				<input
-					id="email"
-					name="email"
-					type="email"
-					value={values.email}
-					onChange={changeHandler}
-					className={
-						errors.email ? "Form__input Form__input--error" : "Form__input"
-					}
-					required
-				/>
-				{errors.email && (
-					<p className="Form__error">{<Translate id={errors.email} />}</p>
-				)}
+				<Layout.Flex column gap="8px" className="Form__field">
+					<label htmlFor="name">
+						<Translate id="contact-name" />
+					</label>
+					<input
+						id="name"
+						name="name"
+						type="text"
+						value={values.name}
+						onChange={changeHandler}
+						className={
+							errors.name ? "Form__input Form__input--error" : "Form__input"
+						}
+						required
+					/>
+					{errors.name && (
+						<p className="Form__error">{<Translate id={errors.name} />}</p>
+					)}
+				</Layout.Flex>
+				<Layout.Flex column gap="8px" className="Form__field">
+					<label htmlFor="email">
+						<Translate id="contact-email" />
+					</label>
+					<input
+						id="email"
+						name="email"
+						type="email"
+						value={values.email}
+						onChange={changeHandler}
+						className={
+							errors.email ? "Form__input Form__input--error" : "Form__input"
+						}
+						required
+					/>
+					{errors.email && (
+						<p className="Form__error">{<Translate id={errors.email} />}</p>
+					)}
+				</Layout.Flex>
 				<label htmlFor="message">
 					<Translate id="contact-message" />
 				</label>
